62f1dde2ea46bf14a39c07845b2c66deb5655120
Artem Pushkarev <pushkarev_av@inbox.ru>
2022-07-29 09:29:29 +0300

    add: code in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let b = '';
+    strN = String (Math.abs(n));
+    for (let i = strN.length - 1; i >= 0; i--){
+        console.log(strN[i]);
+        b = b + strN[i];
+    }
+    return +b
 }
